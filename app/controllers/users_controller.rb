db1247237318d3cf14c9e3fb58a8b5c030be0743
Melissa Williams <melissa@brandmeetscopy.com>
2020-03-02 17:25:07 -0800

    added create, show functionality for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,32 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
     #get all users and show their emails
   end
 
   def new
-    
+    @user = User.new 
   end
 
   def create
+
+    @user = User.new(user_params)
+        @user.save
+        redirect_to user_path(@user)
     # TODO: create user
     #redirect to users path for that user 
-
+   
   end
 
   def show 
+    @user = User.find(params[:id])
+
     # TODO: show user email 
   end
+private 
 
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
